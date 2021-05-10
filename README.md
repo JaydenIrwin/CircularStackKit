57ca094a462cfcc70364bef66d32ce43ef69a508
Jayden Irwin <me@jaydenirwin.com>
2021-05-10 15:45:59 -0400

    view extractor

--- README.md
+++ README.md
@@ -4,7 +4,4 @@ A SwiftUI view that organizes it's children on a circular path.
 
 ## Flexible Options:
 - Set start and end angles of the path to layout children on only the top half of a circle for example.
-- Use the angle given in the closure to rotate children along with the circular path, if you wish.
-
-## Usage
-The content closure provides you (index: Int, rotationAngle: Angle) for each child view you should return. Use a switch statement in here to provide different views for each index. Optionally use the angle to rotate elements of your child views.
+- Use the child rotation modes to rotate children along with the circular path, or not.
--- Sources/CircularStackKit/CircularStackView.swift
+++ Sources/CircularStackKit/CircularStackView.swift
@@ -1,73 +1,90 @@
 import SwiftUI
+import ViewExtractor
 
-public struct CircularStackView<Content: View>: View {
+public struct CircularStackView: View {
     
-    struct ChildPosition: Identifiable {
-        let index: Int
-        var id: Int {
-            index
-        }
+    struct ChildPosition {
         let offset: CGSize
-        let rotationAngle: Angle
+        let upIsOutwardAngle: Angle
+    }
+    public enum ChildRotationMode {
+        case none, upIsOutward, upIsInward
     }
     
+    private var views: [AnyView]
     var startAngle: CGFloat
     var endAngle: CGFloat
     var radius: CGFloat
+    var childRotationMode: ChildRotationMode
     let childPositions: [ChildPosition]
-    
-    let content: (Int, Angle) -> Content
     
     public var body: some View {
         ZStack {
-            ForEach(childPositions) { pos in
-                content(pos.index, pos.rotationAngle)
-                    .offset(pos.offset)
+            ForEach(views.indices) { index in
+                views[index]
+                    .rotationEffect({
+                        switch childRotationMode {
+                        case .none:
+                            return .zero
+                        case .upIsOutward:
+                            return childPositions[index].upIsOutwardAngle
+                        case .upIsInward:
+                            return childPositions[index].upIsOutwardAngle - Angle(radians: .pi)
+                        }
+                    }())
+                    .offset(childPositions[index].offset)
             }
         }
         .frame(width: radius*2, height: radius*2)
     }
     
-    public init(childCount: Int, startAngle: CGFloat = -.pi*1.5, endAngle: CGFloat = .pi/2, radius: CGFloat, @ViewBuilder content: @escaping (Int, Angle) -> Content) {
+    public init<Content: View>(startAngle: CGFloat = -.pi*1.5, endAngle: CGFloat = .pi/2, radius: CGFloat, childRotation: ChildRotationMode = .none, @ViewBuilder content: () -> Content) {
+        self.views = [AnyView(content())]
         self.startAngle = startAngle
         self.endAngle = endAngle
         self.radius = radius
+        self.childRotationMode = childRotation
         
-        var denominator = CGFloat(childCount)
+        let offsetAngle = (startAngle + endAngle) / 2
+        let offset = CGSize(width: radius * cos(offsetAngle), height: radius * sin(offsetAngle))
+        let rotationAngle = Double(offsetAngle) + .pi/2
+        self.childPositions = [ChildPosition(offset: offset, upIsOutwardAngle: Angle(radians: rotationAngle))]
+    }
+    
+    public init<Views>(startAngle: CGFloat = -.pi*1.5, endAngle: CGFloat = .pi/2, radius: CGFloat, childRotation: ChildRotationMode = .none, @ViewBuilder content: ViewContent<Views>) {
+        self.views = ViewExtractor.getViews(from: content)
+        self.startAngle = startAngle
+        self.endAngle = endAngle
+        self.radius = radius
+        self.childRotationMode = childRotation
+        
+        var denominator = CGFloat(views.count)
         let diffFrom2PI = abs(abs(endAngle - startAngle) - .pi*2)
         if 0.01 < diffFrom2PI {
             denominator -= 1.0
         }
         let angleStep = (1.0 / denominator) * (endAngle - startAngle)
         var positions = [ChildPosition]()
-        for index in 0..<childCount {
+        for index in 0..<views.count {
             let offsetAngle = startAngle + angleStep * CGFloat(index)
             let offset = CGSize(width: radius * cos(offsetAngle), height: radius * sin(offsetAngle))
             let rotationAngle = Double(offsetAngle) + .pi/2
-            positions.append(ChildPosition(index: index, offset: offset, rotationAngle: Angle(radians: rotationAngle)))
+            positions.append(ChildPosition(offset: offset, upIsOutwardAngle: Angle(radians: rotationAngle)))
         }
         
         self.childPositions = positions
-        self.content = content
     }
+    
 }
 
 struct CircularStackView_Previews: PreviewProvider {
     static var previews: some View {
-        CircularStackView(childCount: 5, radius: 100.0) { index, angle in
-            switch index {
-            case 0:
-                Text("0")
-                    .rotationEffect(angle) // If you want this to follow the angle of the circle
-            case 1:
-                Text("1")
-            case 2:
-                Text("2")
-            case 3:
-                Text("3")
-            default:
-                Text("...")
-            }
+        CircularStackView(radius: 100.0) {
+            Text("0")
+            Text("1")
+            Text("2")
+            Text("3")
+            Text("...")
         }
     }
 }
